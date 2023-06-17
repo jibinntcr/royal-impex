e11ecc7c59b2aac95fbd7b6f2c989fb160559eaa
sai-na <saipkmm100@gmail.com>
2023-06-17 16:04:02 +0530

    image slide -  pattern glass

---
+++ js/products/patternGlass.js
@@ -0,0 +1,24 @@
+let imageAnimPatternGlass = document.getElementById("image-animate-patternGlass");
+
+let imageArrayPatternGlass = [
+    "../../images/products/PatternGlass/01.jpeg",
+    "../../images/products/PatternGlass/02.jpeg",
+    "../../images/products/PatternGlass/03.jpeg",
+    "../../images/products/PatternGlass/04.jpeg",
+    "../../images/products/PatternGlass/05.jpeg",
+];
+
+let imageIndexPatternGlass = 0;
+
+const startImagePatternGlass = () => {
+    imageAnimPatternGlass.setAttribute("src", imageArrayPatternGlass[imageIndexPatternGlass]);
+    imageIndexPatternGlass++;
+    if (imageIndexPatternGlass >= imageArrayPatternGlass.length) {
+        imageIndexPatternGlass = 0;
+    }
+};
+
+setInterval(startImagePatternGlass, 2800);
+
+
+
